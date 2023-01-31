93ba0a899f763abdadb6096aebfa0c152304814c
ljx <1445101363@qq.com>
2023-01-31 17:05:15 +0800

    add convert_to_onnx

---
+++ convert_to_onnx.py
@@ -0,0 +1,75 @@
+import sys
+
+sys.path.append('core')
+
+import argparse
+import glob
+import numpy as np
+import torch
+from tqdm import tqdm
+from pathlib import Path
+from raft_stereo import RAFTStereo
+from utils import InputPadder
+from PIL import Image
+from matplotlib import pyplot as plt
+
+DEVICE = 'cuda'
+
+
+def load_image(imfile):
+    img = np.array(Image.open(imfile)).astype(np.uint8)
+    img = torch.from_numpy(img).permute(2, 0, 1).float()
+    return img[None].to(DEVICE)
+
+
+def demo(args):
+    model = torch.nn.DataParallel(RAFTStereo(args), device_ids=[0])
+    model.load_state_dict(torch.load(args.restore_ckpt))
+
+    model = model.module
+    model.to(DEVICE)
+    model.eval()
+
+    onnx_input_L = torch.rand(1, 3, 200, 320)
+    onnx_input_R = torch.rand(1, 3, 200, 320)
+    onnx_input_L = onnx_input_L.to("cuda:0")
+    onnx_input_R = onnx_input_R.to("cuda:0")
+    # onnx_input = torch.cat((onnx_input_L, onnx_input_R), 1)
+    torch.onnx.export(model,
+                      (onnx_input_L, onnx_input_R),
+                      "{}.onnx".format("RAFT-Stereo"),
+                      # where to save the model (can be a file or file-like object)
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=12,  # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['left', 'right'],  # the model's input names
+                      output_names=['output'])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--restore_ckpt', help="restore checkpoint", required=True)
+    parser.add_argument('--save_numpy', action='store_true', help='save output as numpy arrays')
+    parser.add_argument('-l', '--left_imgs', help="path to all first (left) frames",
+                        default="datasets/Middlebury/MiddEval3/testH/*/im0.png")
+    parser.add_argument('-r', '--right_imgs', help="path to all second (right) frames",
+                        default="datasets/Middlebury/MiddEval3/testH/*/im1.png")
+    parser.add_argument('--output_directory', help="directory to save output", default="demo_output")
+    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
+    parser.add_argument('--valid_iters', type=int, default=32, help='number of flow-field updates during forward pass')
+
+    # Architecture choices
+    parser.add_argument('--hidden_dims', nargs='+', type=int, default=[128] * 3,
+                        help="hidden state and context dimensions")
+    parser.add_argument('--corr_implementation', choices=["reg", "alt", "reg_cuda", "alt_cuda"], default="reg",
+                        help="correlation volume implementation")
+    parser.add_argument('--shared_backbone', action='store_true',
+                        help="use a single backbone for the context and feature encoders")
+    parser.add_argument('--corr_levels', type=int, default=4, help="number of levels in the correlation pyramid")
+    parser.add_argument('--corr_radius', type=int, default=4, help="width of the correlation pyramid")
+    parser.add_argument('--n_downsample', type=int, default=2, help="resolution of the disparity field (1/2^K)")
+    parser.add_argument('--slow_fast_gru', action='store_true', help="iterate the low-res GRUs more frequently")
+    parser.add_argument('--n_gru_layers', type=int, default=3, help="number of hidden GRU levels")
+
+    args = parser.parse_args()
+
+    demo(args)
